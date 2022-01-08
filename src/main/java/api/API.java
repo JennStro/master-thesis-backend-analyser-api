5aeca3da3f0e8de8219e1e44c002f4161f697b73
Jenny Strømmen <jennys1994@gmail.com>
2022-01-08 16:25:17 +0100

    First commit

---
+++ src/main/java/api/API.java
@@ -0,0 +1,36 @@
+package api;
+
+import master.thesis.backend.analyser.Analyser;
+import master.thesis.backend.errors.BaseError;
+import master.thesis.backend.errors.BugReport;
+
+import static spark.Spark.*;
+
+public class API {
+
+    public static void main(String[] args) {
+        get("/health", (req, res) -> {
+            return new Analyser().analyse("OK");
+        });
+
+        post("/analyse", (request, response) -> {
+            BugReport report = new Analyser().analyse(request.body());
+            if (!report.getBugs().isEmpty()) {
+                BaseError error = report.getBugs().get(0);
+                if (error.getLineNumber() == -1) {
+                    if (error.getSuggestion().isPresent()) {
+                        return "In class " + error.getContainingClass() + " " + error.getWhat() + "\n" + error.getSuggestion().get();
+                    }
+                    return "In class " + error.getContainingClass() + " " + error.getWhat();
+                } else {
+                    if (error.getSuggestion().isPresent()) {
+                        return "In class " + error.getContainingClass() + ", on line " + error.getLineNumber() + " " + error.getWhat() + "\n" + error.getSuggestion().get();
+                    }
+                    return "In class " + error.getContainingClass() + ", on line " + error.getLineNumber() + " " + error.getWhat();
+                }
+
+            }
+            return "Found no errors!";
+        });
+    }
+}
